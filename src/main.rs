fe65c62828d72e79143282e0fabb2d78c66d3c41
Felix Stegmaier <stegmaier.felix@gmail.com>
2021-12-24 00:12:27 +0100

    extend example

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,22 @@ fn main() {
                 return new Pair(newfst, this.snd);
             }
         }
+
+        class D extends Object {
+            Pair left_pair;
+            Pair right_pair;
+            D(Pair left_pair, Pair right_pair) {
+                super();
+                this.left_pair=left_pair;
+                this.right_pair=right_pair;
+            }
+            Object leftmost() {
+                return this.left_pair.fst;
+            }
+            Pair left_setfst(Object n) {
+                return this.left_pair.setfst(n);
+            }
+        }
     ").expect("parsing failed");
     println!("AST {:#?}", &ast);
 }
