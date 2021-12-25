df5841224c26ec56354bc5c18afa9d2b4b635375
Felix Stegmaier <stegmaier.felix@gmail.com>
2021-12-25 18:11:48 +0100

    implement e-invkarg

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -104,22 +104,29 @@ pub fn eval_step(ct: &ClassTable, term: Term) -> Result<Term> {
                     .clone())
             }
             // E-Field
-            _ => todo!(),
+            t => Ok(FieldAccess {
+                field,
+                object_term: eval_step(ct, t)?.boxed(),
+            }
+            .into_term()),
         },
         Term::MethodCall(MethodCall {
-            arg_terms,
+            mut arg_terms,
             method_name,
             object_term,
         }) => match *object_term {
             // E-Invk-New
-            Term::NewCall(nc) if nc.has_only_value_args() => {
+            // NOTE: object terms be also only be values here
+            Term::NewCall(nc)
+                if nc.has_only_value_args() && arg_terms.iter().all(|u| u.is_value()) =>
+            {
                 let method_body = ct.method_body(&method_name, &nc.class_name).ok_or(anyhow!(
                     "Method `{}` in class `{}` not defined",
                     &method_name.0,
                     &nc.class_name.0
                 ))?;
                 let this_field = FieldName("this".into());
-                let replacements = iter::once((&this_field, Term::NewCall(nc)))
+                let replacements = iter::once((&this_field, nc.into_term()))
                     .chain(
                         method_body
                             .args
@@ -129,7 +136,30 @@ pub fn eval_step(ct: &ClassTable, term: Term) -> Result<Term> {
                     .collect();
                 Ok(substitute_many(*method_body.return_term, replacements))
             }
-            _ => todo!(),
+            // E-InvkArg
+            v if v.is_value() => {
+                // NOTE(unwrap): safe because of previous match arm
+                let (first_non_value, _) = arg_terms
+                    .iter()
+                    .enumerate()
+                    .find(|(_, t)| !t.is_value())
+                    .unwrap();
+                arg_terms[first_non_value] =
+                    eval_step(ct, *arg_terms[first_non_value].clone())?.boxed();
+                Ok(MethodCall {
+                    arg_terms,
+                    method_name,
+                    object_term: v.boxed(),
+                }
+                .into_term())
+            }
+            // E-InvkRecv
+            t => Ok(MethodCall {
+                arg_terms,
+                method_name,
+                object_term: eval_step(ct, t)?.boxed(),
+            }
+            .into_term()),
         },
         Term::Cast(Cast {
             term,
@@ -137,12 +167,15 @@ pub fn eval_step(ct: &ClassTable, term: Term) -> Result<Term> {
         }) => match *term {
             // E-CastNew
             Term::NewCall(nc) if nc.has_only_value_args() => {
-                if ct.is_subtype(&nc.class_name, &to_class_name).ok_or(anyhow!(
-                    "Class `{}` or `{}` not defined",
-                    &nc.class_name.0,
-                    &to_class_name.0
-                ))? {
-                    Ok(Term::NewCall(nc))
+                if ct
+                    .is_subtype(&nc.class_name, &to_class_name)
+                    .ok_or(anyhow!(
+                        "Class `{}` or `{}` not defined",
+                        &nc.class_name.0,
+                        &to_class_name.0
+                    ))?
+                {
+                    Ok(nc.into_term())
                 } else {
                     bail!(
                         "Cast failed for class `{}` to `{}`",
